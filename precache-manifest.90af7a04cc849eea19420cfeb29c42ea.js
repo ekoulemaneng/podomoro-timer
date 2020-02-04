d7a56f30f81ab8e3d337604560f7a999dc2abf17
ekoulemaneng <ekoulemaneng@gmail.com>
2020-02-04 08:48:53 +0100

    Updates

---
+++ precache-manifest.90af7a04cc849eea19420cfeb29c42ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "61eb5a4053f714f2194be9385aa2824d",
+    "url": "/podomoro-timer/static/media/alarm.61eb5a40.mp3"
+  },
+  {
+    "revision": "8f508036beb1c8778d9c",
+    "url": "/podomoro-timer/static/js/runtime~main.d6c1f795.js"
+  },
+  {
+    "revision": "c963d271797a64d2804e",
+    "url": "/podomoro-timer/static/js/main.e2e79644.chunk.js"
+  },
+  {
+    "revision": "328ef1e2b1c8229a71c6",
+    "url": "/podomoro-timer/static/js/2.a34f7281.chunk.js"
+  },
+  {
+    "revision": "c963d271797a64d2804e",
+    "url": "/podomoro-timer/static/css/main.27618c48.chunk.css"
+  },
+  {
+    "revision": "328ef1e2b1c8229a71c6",
+    "url": "/podomoro-timer/static/css/2.a186d080.chunk.css"
+  },
+  {
+    "revision": "4ba87703bbdf296f75e3d1c896ffea6e",
+    "url": "/podomoro-timer/index.html"
+  }
+];
